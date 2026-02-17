1da560a13b5a59698c3fe37ec77ac756a5422861
Doroftei <93725298+doroftei2006@users.noreply.github.com>
2026-02-17 21:47:00 +0000

    Add files via upload

--- 1.js
+++ 1.js
@@ -3,25 +3,37 @@
 document.addEventListener("DOMContentLoaded", function () {
 
   const boxes = document.querySelectorAll(".box");
-  let clickCount = 0;
-  let currentIndex = 0;
+
+  let clickCount = parseInt(localStorage.getItem("clickCount")) || 0;
+  let currentIndex = parseInt(localStorage.getItem("currentIndex")) || 0;
+
+  const maxLevels = 3;
+
+  boxes.forEach(box => box.classList.remove("active"));
+  boxes[currentIndex].classList.add("active");
 
   boxes.forEach((box) => {
     box.addEventListener("click", function () {
 
       clickCount++;
-      console.log("Кликов:", clickCount);
+      console.log("Кліків:", clickCount);
 
       if (clickCount % 10 === 0) {
+
         boxes[currentIndex].classList.remove("active");
 
         currentIndex++;
-        if (currentIndex >= boxes.length) {
+
+        if (currentIndex >= maxLevels) {
           currentIndex = 0;
+          clickCount = 0;
         }
 
         boxes[currentIndex].classList.add("active");
       }
+
+      localStorage.setItem("clickCount", clickCount);
+      localStorage.setItem("currentIndex", currentIndex);
 
     });
   });
